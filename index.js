f0e05b16d4d377e798a749140de68f51b85379ba
Daria <Tr3deprinter@yandex.ru>
2022-09-07 20:10:24 +0300

    qdf

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ story.hasMany(storybl);
 story.hasMany(storylin);
 
 bot.start (async (ctx) =>{
-  if (ctx.message.from.is_bot = true){
+  /*if (ctx.message.from.is_bot = true){
     await ctx.telegram.kickChatMember(ctx.chat.id, ctx.message.from.id)
-  }
+  }*/
   await ctx.reply(`Здравствуйте, ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!`)
 })
 

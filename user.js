d566a3458eee68efc339fe60e54d5111767ad823
Daria <Tr3deprinter@yandex.ru>
2022-10-01 16:24:10 +0300

    qdf

--- user.js
+++ user.js
@@ -3,7 +3,7 @@ const {DataTypes} = require('sequelize')
 
 const user = sequelize.define ('user', {
     authId: {type: DataTypes.BIGINT, unique: false},
-    last_message_time: {type: DataTypes.TIME},
+    last_message_time: {type: DataTypes.BIGINT},
     isbot: {type: DataTypes.BOOLEAN},
     ban: {type: DataTypes.BOOLEAN}
 })

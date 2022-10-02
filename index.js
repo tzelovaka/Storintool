9a32e208532108f2c84006ab8ea377ed0f3a62bb
Daria <Tr3deprinter@yandex.ru>
2022-10-02 15:43:11 +0300

    qdf

--- index.js
+++ index.js
@@ -130,40 +130,46 @@ baseSave.on ('text', async (ctx)=>{
   const t = await sequelize.transaction();
 
   try{
-  //const res = await sequelize.transaction(async (t) => {
+  const res = await sequelize.transaction(async (t) => {
     const query = await story.create({
     name: `${ctx.wizard.state.data.storyName}`,
     desc: `${ctx.wizard.state.data.storyDesc}`,
     authId: ctx.message.from.id,
     release: false
   }, { transaction: t });
+})
 } catch (e) {
   await t.rollback();
   await ctx.reply ('⚠Ошибка!');
   return ctx.scene.leave()
 }
 await t.commit('commit');
-  /*const { count, rows } = await story.findAndCountAll({where: {
+
+const f = await sequelize.transaction();
+try{
+  const res = await sequelize.transaction(async (f) => {
+  const { count, rows } = await story.findAndCountAll({where: {
   authId: ctx.message.from.id,
   release: false
 }});
 
   let c = count - 1;
-*/try{
+
   const quer = await storybl.create({
     linid: 0,
     bl: `${ctx.wizard.state.data.baseSave}`,
     authId: ctx.message.from.id,
-    storyId: query.id,
+    storyId: rows[c].id,
     release: false
-}, { transaction: t });
+}, { transaction: f });
+  })
 }catch (e) {
-  await t.rollback();
+  await f.rollback();
   await ctx.reply ('⚠Ошибка!');
   return ctx.scene.leave()
 }
 //})
-await t.commit('commit');
+await f.commit('commit');
 
   await ctx.reply ('Вы успешно добавили первый блок своей будущей истории.');
   return ctx.scene.leave()

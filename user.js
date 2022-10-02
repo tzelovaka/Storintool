8ecfdbc11265a39dbefbe73b8ef1ce0d6d40845a
Daria <Tr3deprinter@yandex.ru>
2022-10-02 13:32:54 +0300

    qdf

--- user.js
+++ user.js
@@ -5,8 +5,8 @@ const user = sequelize.define ('user', {
     authId: {type: DataTypes.BIGINT, unique: false},
     last_message_time: {type: DataTypes.BIGINT},
     count: {type: DataTypes.INTEGER, allowNull: false, defaultValue: 0, unique: false},
-    isbot: {type: DataTypes.BOOLEAN},
-    ban: {type: DataTypes.BOOLEAN}
+    isbot: {type: DataTypes.BOOLEAN, defaultValue: false},
+    ban: {type: DataTypes.BOOLEAN, defaultValue: false}
 })
 
 module.exports = user;

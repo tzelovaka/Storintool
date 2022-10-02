1d44874e250c004ddd76811b141daca9415ef946
Daria <Tr3deprinter@yandex.ru>
2022-10-02 15:59:51 +0300

    qdf

--- index.js
+++ index.js
@@ -145,30 +145,28 @@ baseSave.on ('text', async (ctx)=>{
 }
 await t.commit('commit');
 
-const f = await sequelize.transaction();
 try{
-  const res = await sequelize.transaction(async (f) => {
   const { count, rows } = await story.findAndCountAll({where: {
     authId: ctx.message.from.id,
     release: false
   }});
-  
     let c = count - 1;
+
+  const res = await sequelize.transaction(async (t) => {
   const quer = await storybl.create({
     linid: 0,
     bl: `${ctx.wizard.state.data.baseSave}`,
     authId: ctx.message.from.id,
     storyId: rows[c].id,
     release: false
-}, { transaction: f });
+}, { transaction: t });
   })
 }catch (e) {
-  await f.rollback();
+  await t.rollback();
   await ctx.reply ('⚠Ошибка!');
   return ctx.scene.leave()
 }
-//})
-await f.commit('commit');
+await t.commit('commit');
 
   await ctx.reply ('Вы успешно добавили первый блок своей будущей истории.');
   return ctx.scene.leave()

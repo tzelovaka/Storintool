2e608a0970d9306e5efb1696af8df85ed8887d8e
Daria <Tr3deprinter@yandex.ru>
2022-10-02 16:23:13 +0300

    qdf

--- index.js
+++ index.js
@@ -128,39 +128,39 @@ const baseSave = new Composer()
 baseSave.on ('text', async (ctx)=>{
   ctx.wizard.state.data.baseSave = ctx.message.text;
 
-  const t = await sequelize.transaction();
+  /*const t = await sequelize.transaction();*/
   try{
-  const res = await sequelize.transaction(async (t) => {
+  /*const res = await sequelize.transaction(async (t) => {*/
     const query = await story.create({
     name: `${ctx.wizard.state.data.storyName}`,
     desc: `${ctx.wizard.state.data.storyDesc}`,
     authId: ctx.message.from.id,
     release: false
-  }, { transaction: t });
+  }/*, { transaction: t }*/);/*
 })
 } catch (e) {
   await t.rollback();
   await ctx.reply ('⚠Ошибка!');
   return ctx.scene.leave()
-}
-const f = await sequelize.transaction();
-try{
+}*/
+/*const f = await sequelize.transaction();
+try{*/
     const { count, rows } = await story.findAndCountAll({where: {
       authId: ctx.message.from.id,
       release: false}});
     let c = count - 1;
-    const t = await sequelize.transaction();
-    const result = await sequelize.transaction(async (f) => {
-    const query = await storybl.create({
+    //const t = await sequelize.transaction();
+    //const result = await sequelize.transaction(async (f) => {
+    const quer = await storybl.create({
     linid: 0,
     bl: `${ctx.wizard.state.data.baseSave}`,
     authId: ctx.message.from.id,
     storyId: rows[c].id,
     release: false
-  }, { transaction: f });
-})
+  }/*, { transaction: f }*/);
+//})
 }catch(e){
-  await f.rollback();
+  //await f.rollback();
   await ctx.reply ('⚠Ошибка!');
   return ctx.scene.leave()
 }

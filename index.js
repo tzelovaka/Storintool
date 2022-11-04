a451b360b309fce599042183fbd24197b0910144
Daria <Tr3deprinter@yandex.ru>
2022-11-05 01:28:19 +0300

    serverfirst

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 const { BOT_TOKEN} = process.env;
 const bot = new Telegraf(BOT_TOKEN);
 const flagBtn = new CallbackData('flagBtn', ['number', 'action']);
-
+const web_link = 'https://storinter.herokuapp.com/'
 
 if (BOT_TOKEN === undefined) {
   throw new Error('BOT_TOKEN must be provided!')
@@ -71,6 +71,19 @@ bot.start (async (ctx) =>{
 /public - публикация истории.`)
   }
   )
+
+  bot.help (async (ctx) =>{
+    /*if (ctx.message.from.is_bot = true){
+      await ctx.telegram.kickChatMember(ctx.chat.id, ctx.message.from.id)
+    }*/
+      await ctx.reply(
+      'Привет', {
+        reply_markup:{
+          keyboard:[[{text: "Тык", web_app: {url: web_link}}]],
+        },
+      })
+    }
+    )
 
 const baseEmpty = new Composer()
 baseEmpty.on ('text', async (ctx)=>{

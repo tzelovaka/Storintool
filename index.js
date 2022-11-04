9ec89e8d189f4e869c2a18bf818af100b4d383ea
Daria <Tr3deprinter@yandex.ru>
2022-11-04 21:41:00 +0300

    serverfirst

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 const { BOT_TOKEN} = process.env;
 const bot = new Telegraf(BOT_TOKEN);
 const flagBtn = new CallbackData('flagBtn', ['number', 'action']);
-
+const webAppUrl = 'https://storinter.herokuapp.com/'
 
 if (BOT_TOKEN === undefined) {
   throw new Error('BOT_TOKEN must be provided!')
@@ -68,10 +68,13 @@ bot.start (async (ctx) =>{
 /edit - редактирование текста;
 /visualization - добавление картинок или эмодзи-кнопок;
 /delete - удаление истории, сюжетной ветви или картинок;
-/public - публикация истории.`), Markup.keyboard(
-  [
-    [['Схема']]
-    ])
+/public - публикация истории.`), {
+  reply_markup:{
+    keyboard:[
+      [{text: 'Схема', web_app: {url: webAppUrl}}]
+    ]
+  }
+}
   }
   )
 
